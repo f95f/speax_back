a1b87f77eadd0cecd2a84ff7a2a8fdc3c31a283b
f95f <95felipefernandes@gmail.com>
2024-02-16 00:28:11 -0300

    feat: adds User class.

---
+++ src/main/java/com/C/speaxs/domain/model/User.java
@@ -0,0 +1,59 @@
+package com.C.speaxs.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @NotBlank
+    @Size(max = 128)
+    private String firstName;
+
+    @Size(max = 128)
+    private String lastName;
+
+    @NotBlank
+    @Email
+    @Size(max = 128)
+    private String email;
+
+    @NotBlank
+    @Size(max = 128)
+    private String password;
+
+    @Size(max = 20)
+    private String phone;
+
+    @Size(max = 512)
+    private String bio;
+
+    private boolean status;
+
+    @NotNull
+    private Date birthDate;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+
+}
