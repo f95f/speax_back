994dabbc008bc40420a088323fc44f2d2d938cf4
f95f <95felipefernandes@gmail.com>
2024-02-16 00:09:07 -0300

    feat: initial commit

---
+++ src/main/java/com/C/speaxs/SpeaxsApplication.java
@@ -0,0 +1,13 @@
+package com.C.speaxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeaxsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeaxsApplication.class, args);
+	}
+
+}
